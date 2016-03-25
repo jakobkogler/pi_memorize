75f3d1f5fbf682d86b37242313a92b46595c3408
jakobkogler <jakob.kogler@gmail.com>
2016-03-25 14:31:15 +0100

    Compute pi with Machin's formula (faster than BBP)

--- compute_pi.py
+++ compute_pi.py
@@ -24,6 +24,38 @@ class ComputePi:
         pi = str(pi)[:-19]
         return pi
 
+    @staticmethod
+    def arctan_euler(x, one=1000000):
+        """Calculate arctan(1/x) using euler's accelerated formula.
+
+        Based on http://www.craig-wood.com/nick/articles/pi-machin/"""
+        x_squared = x * x
+        x_squared_plus_1 = x_squared + 1
+        term = (x * one) // x_squared_plus_1
+        total = term
+        two_n = 2
+        while 1:
+            divisor = (two_n+1) * x_squared_plus_1
+            term *= two_n
+            term += divisor // 2    # round the division
+            term = term // divisor
+            if term == 0:
+                break
+            total += term
+            two_n += 2
+        return total
+
+    @staticmethod
+    def machin_euler(one):
+        """Compute pi using Machin's formula.
+
+        Based on http://www.craig-wood.com/nick/articles/pi-machin/"""
+        one *= 10**20
+        pi = 4*(4*ComputePi.arctan_euler(5, one) - ComputePi.arctan_euler(239, one))
+        pi //= 10**20
+        return '3.{}'.format(str(pi)[1:])
+
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calculates pi.')
@@ -32,4 +64,5 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     pi_computer = ComputePi()
-    print(pi_computer.BBP(args.precision))
+
+    print(pi_computer.machin_euler(10**args.precision))
